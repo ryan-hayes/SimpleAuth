ce45eb062e514dcbd4d34be378d94a59779e3e5e
Ryan Hayes <hayes1rp@vt.edu>
2017-06-21 02:49:03 -0400

    Updated connection string params

--- modules/auth/db-connect.php
+++ modules/auth/db-connect.php
@@ -2,6 +2,6 @@
 <!-- Last updated by Ryan Hayes on 6/5/2017 -->
 
 <?php
-	$db = mysqli_connect('localhost','root','','lumen-auth')
+	$db = mysqli_connect('localhost','root','','auth')
 	or die('Error connecting to MySQL server.');
 ?>
